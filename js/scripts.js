508fc346d65dcb51e901b4e03ed1c12680c66d24
sarah leahy <sarah leahy>
2017-01-20 11:35:56 -0700

    will take input and output the count

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,23 @@
 // back end logic
-var number = ""
-function(count) {
-  
+// var answerr = [];
+// var inputValue = "";
+
+function pingPong(inputValue_) {
+  var countArray = [];
+  for ( var i=1; i<=10; i++) {
+   countArray.push(i);
+  //  console.log("the output is" + countArray);
+  }
+return countArray;
 }
-
-
 // font end logic
-$(document).ready(function(){
-  count(number);
+$(document).ready(function() {
+ $("form#num-form").submit(function(event) {
+   event.preventDefault();
+  //  $("#output").empty();
+   var inputValue = $("input#tbox").val();
+   var solution = pingPong(inputValue);
+   $("#output").append("<p>" + solution + "</p>");
+  //  countArray = [];
+ });
 });
